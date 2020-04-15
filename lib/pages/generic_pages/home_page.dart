040cb8c263fb607c3df7f0d3bce38a7dab803c54
SolarMotion <ywc92@hotmail.com>
2020-04-15 12:31:38 +0800

    aa

--- lib/pages/generic_pages/home_page.dart
+++ lib/pages/generic_pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_beanbag_consumer_app_v1/resources/theme_designs.dart';
 
 import './../../widgets/custom_drawer.dart';
 import './../../widgets/custom_app_bar.dart';
@@ -18,6 +19,31 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: Text('Coming soon.'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud_download),
+            title: Text('DOWNLOAD'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            title: Text('COUPONS'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.monetization_on),
+            title: Text('FUNDRAISING'),
+          ),
+        ],
+        currentIndex: 2,
+        selectedItemColor: Colors.black,
+        backgroundColor: ThemeDesign.appPrimaryColor,
+        unselectedItemColor: Colors.white,        
       ),
     );
   }
